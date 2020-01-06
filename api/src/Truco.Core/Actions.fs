8dab0ba06b830e07a3e8f9ac366ec35c6a6aee0e
Cássio Farias Machado <cassiofariasmachado@yahoo.com>
2020-01-05 21:48:21 -0300

    refact(api): refactor the domain

--- api/src/Truco.Core/Actions.fs
+++ api/src/Truco.Core/Actions.fs
@@ -6,66 +6,56 @@ open MoreLinq
 
 module Actions =
 
-    let cardValue card =
-        cardValues.Item card
+    let cardValue card = cardValues.Item card
 
     let isBlackCard card =
         let value = cardValue card
 
         match value with
-        | 14 | 13 | 12 | 11 -> false
+        | 14
+        | 13
+        | 12
+        | 11 -> false
         | _ -> true
 
-    let shuffleDeck =
-        MoreEnumerable.RandomSubset(cardValues.Keys, cardValues.Keys.Count)
-        |> List.ofSeq:Deck
+    let shuffleDeck: Deck = MoreEnumerable.RandomSubset(cardValues.Keys, cardValues.Keys.Count) |> List.ofSeq
 
-    let checkTurnWinner (playerOne:Player, cardOne) (playerTwo:Player, cardTwo) =
+    let checkTurnWinner (playerOne: Player, cardOne) (playerTwo: Player, cardTwo) =
         match (cardValue cardOne, cardValue cardTwo) with
-        | (valueOne, valueTwo)
-            when valueOne > valueTwo
-                -> Some(playerOne)
-        | (valueOne, valueTwo)
-            when valueOne < valueTwo
-                -> Some(playerTwo)
+        | (valueOne, valueTwo) when valueOne > valueTwo -> Some(playerOne)
+        | (valueOne, valueTwo) when valueOne < valueTwo -> Some(playerTwo)
         | _ -> None
 
     let removeCard player card =
         let hand = player.Hand |> List.filter ((<>) card)
         { player with Hand = hand }
 
-    let addTurnToTurnHistory round turn =
-        { round with TurnHistory = turn::round.TurnHistory }
+    let addTurnToTurnHistory round turn = { round with TurnHistory = turn :: round.TurnHistory }
 
     let addTurnToRoundHistory _match turn =
         let updatedLastRound = addTurnToTurnHistory _match.RoundHistory.Head turn
-        { _match with RoundHistory = updatedLastRound::_match.RoundHistory }
+        { _match with RoundHistory = updatedLastRound :: _match.RoundHistory }
 
-    let addPoint player =
-        { player with Points = player.Points + 1 }
-    
+    let addPoint player = { player with Points = player.Points + 1 }
+
     let turn _match playerOneCard playerTwoCard =
         let playerOne = removeCard _match.PlayerOne.Player playerOneCard
         let playerTwo = removeCard _match.PlayerTwo.Player playerTwoCard
 
-        let winner = checkTurnWinner (playerOne, playerOneCard) (playerTwo, playerTwoCard) 
-        
+        let winner = checkTurnWinner (playerOne, playerOneCard) (playerTwo, playerTwoCard)
+
         let playerOneMove = createPlayerMove playerOne playerOneCard
         let playerTwoMove = createPlayerMove playerTwo playerTwoCard
-        let turn = createTurn playerOneMove playerTwoMove (TurnWinner winner)
+        let turn = createTurn playerOneMove playerTwoMove winner
 
         let updatedMatchWithTurn = addTurnToRoundHistory _match turn
 
         match winner with
-        | Some winner when winner = playerOne
-            -> printf "PlayerOne winner!"
-        | Some winner when winner = playerTwo 
-            -> printf "PlayerTwo winner!"
+        | Some winner when winner = playerOne -> printf "PlayerOne winner!"
+        | Some winner when winner = playerTwo -> printf "PlayerTwo winner!"
         | _ -> printf "Draw"
 
-        {
-            updatedMatchWithTurn with 
-                PlayerOne = { updatedMatchWithTurn.PlayerOne with Player = playerOne }; 
-                PlayerTwo = { updatedMatchWithTurn.PlayerTwo with Player = playerTwo }; 
-        }
-    
+        { updatedMatchWithTurn with
+              PlayerOne = { updatedMatchWithTurn.PlayerOne with Player = playerOne }
+              PlayerTwo = { updatedMatchWithTurn.PlayerTwo with Player = playerTwo } }
+ 
--- api/src/Truco.Core/Models.fs
+++ api/src/Truco.Core/Models.fs
@@ -2,134 +2,140 @@
 
 module Models =
 
-    type Suit = 
-        Sword | Club | Gold | Cup
-    
-    type Rank = 
-          Ace | Two | Three | Four | Five | Six 
-        | Seven | Eight | Nine | Ten | Eleven | Twelve
+    type Suit =
+        | Sword
+        | Club
+        | Gold
+        | Cup
+
+    type Rank =
+        | Ace
+        | Two
+        | Three
+        | Four
+        | Five
+        | Six
+        | Seven
+        | Eight
+        | Nine
+        | Ten
+        | Eleven
+        | Twelve
 
     type Card = Rank * Suit
 
     type Hand = Card list
 
-    let createHand cards =
-        match cards with
-        | [] -> None
-        | [ first; second; third ]
-            -> Some([ first; second; third ])
-        | _ -> None
+    let createHand (cards: Card list): Hand = cards
 
-    type Player = {
-        Name: string;
-        Hand: Hand;
-    }
+    type Player =
+        { Name: string
+          Hand: Hand }
 
     let createPlayer name hand =
-        { Name = name; Hand = hand; }
+        { Name = name
+          Hand = hand }
 
     type Deck = Card list
 
-    type PlayersMove = {
-        Player: Player;
-        PlayedCard: Card;
-    }
+    type PlayersMove =
+        { Player: Player
+          PlayedCard: Card }
 
     let createPlayerMove player card =
-        { Player = player; PlayedCard = card; } 
+        { Player = player
+          PlayedCard = card }
 
-    type TurnWinner = TurnWinner of Player option
+    type TurnWinner = Player option
 
-    type Turn = {
-        PlayersMoveOne: PlayersMove;
-        PlayersMoveTwo: PlayersMove;
-        Winner: TurnWinner;
-    }
+    type Turn =
+        { PlayersMoveOne: PlayersMove
+          PlayersMoveTwo: PlayersMove
+          Winner: TurnWinner }
 
     let createTurn playerOneMove playerTwoMove winner =
-        { PlayersMoveOne = playerOneMove; PlayersMoveTwo = playerTwoMove; Winner = winner; }
+        { PlayersMoveOne = playerOneMove
+          PlayersMoveTwo = playerTwoMove
+          Winner = winner }
 
     type RoundWinner = RoundWinner of Player
 
-    type Round = {
-        TurnHistory: Turn list;
-        Winner: RoundWinner option;
-    }
+    type Round =
+        { TurnHistory: Turn list
+          Winner: RoundWinner option }
 
-    type MatchPlayer = {
-        Player: Player
-        Points: int
-    }
+    type MatchPlayer =
+        { Player: Player
+          Points: int }
 
     let createMatchPlayer player =
-        { Player = player; Points = 0 }
+        { Player = player
+          Points = 0 }
 
     type MatchWinner = MatchWinner of Player
 
-    type Match = {
-        PlayerOne: MatchPlayer;
-        PlayerTwo: MatchPlayer;
-        RoundHistory: Round list;
-        Winner: MatchWinner option;
-    }
+    type Match =
+        { PlayerOne: MatchPlayer
+          PlayerTwo: MatchPlayer
+          RoundHistory: Round list
+          Winner: MatchWinner option }
 
-    let createMatch playerOne playerTwo = 
-        { 
-            PlayerOne = createMatchPlayer playerOne; 
-            PlayerTwo = playerTwo;
-            RoundHistory = [{ TurnHistory = List.Empty;  Winner = None; }];
-            Winner = None;
-        }
+    let createMatch playerOne playerTwo =
+        { PlayerOne = createMatchPlayer playerOne
+          PlayerTwo = createMatchPlayer playerTwo
+          RoundHistory = []
+          Winner = None }
 
-    let cardValues = dict[
-        (Ace, Sword),       14;
-        (Ace, Club),        13;
-        (Seven, Sword),     12;
-        (Seven, Gold),      11;
+    let cardValues =
+        dict
+            [ (Ace, Sword), 14
+              (Ace, Club), 13
+              (Seven, Sword), 12
+              (Seven, Gold), 11
 
-        (Three, Sword),     10;
-        (Three, Club),      10;
-        (Three, Gold),      10;
-        (Three, Cup),       10;
+              (Three, Sword), 10
+              (Three, Club), 10
+              (Three, Gold), 10
+              (Three, Cup), 10
 
-        (Two, Sword),       9;
-        (Two, Club),        9;
-        (Two, Gold),        9;
-        (Two, Cup),         9;
+              (Two, Sword), 9
+              (Two, Club), 9
+              (Two, Gold), 9
+              (Two, Cup), 9
 
-        (Ace, Gold),        8;
-        (Ace, Cup),         8;
+              (Ace, Gold), 8
+              (Ace, Cup), 8
 
-        (Twelve, Sword),    7;
-        (Twelve, Club),     7;
-        (Twelve, Gold),     7;
-        (Twelve, Cup),      7;
+              (Twelve, Sword), 7
+              (Twelve, Club), 7
+              (Twelve, Gold), 7
+              (Twelve, Cup), 7
 
-        (Eleven, Sword),    6;
-        (Eleven, Club),     6;
-        (Eleven, Gold),     6;
-        (Eleven, Cup),      6;
+              (Eleven, Sword), 6
+              (Eleven, Club), 6
+              (Eleven, Gold), 6
+              (Eleven, Cup), 6
 
-        (Ten, Sword),       5;
-        (Ten, Club),        5;
-        (Ten, Gold),        5;
-        (Ten, Cup),         5;
+              (Ten, Sword), 5
+              (Ten, Club), 5
+              (Ten, Gold), 5
+              (Ten, Cup), 5
 
-        (Seven, Club),      4;
-        (Seven, Cup),       4;
+              (Seven, Club), 4
+              (Seven, Cup), 4
 
-        (Six, Sword),       3;
-        (Six, Club),        3;
-        (Six, Gold),        3;
-        (Six, Cup),         3;
+              (Six, Sword), 3
+              (Six, Club), 3
+              (Six, Gold), 3
+              (Six, Cup), 3
 
-        (Five, Sword),      2;
-        (Five, Club),       2;
-        (Five, Gold),       2;
-        (Five, Cup),        2;
+              (Five, Sword), 2
+              (Five, Club), 2
+              (Five, Gold), 2
+              (Five, Cup), 2
 
-        (Four, Sword),      1;
-        (Four, Club),       1;
-        (Four, Gold),       1;
-        (Four, Cup),        1;
-    ]
+              (Four, Sword), 1
+              (Four, Club), 1
+              (Four, Gold), 1
+              (Four, Cup), 1 
+            ]
